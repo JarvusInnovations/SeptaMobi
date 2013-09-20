247ad1f1fae52136d2b1275938e2bc956b25a79a
Ryon Coleman <ryon55@gmail.com>
2013-09-20 03:18:42 -0400

    Nuked theme, added static trip TPL and data, some styling for that view

--- app/view/Main.js
+++ app/view/Main.js
@@ -3,42 +3,53 @@ Ext.define('SeptaMobi.view.Main', {
     xtype: 'main',
     requires: [
         'SeptaMobi.view.TripPlanner.Form',
+        'SeptaMobi.view.TripTplTemp',
         'Ext.TitleBar'
     ],
     config: {
         tabBarPosition: 'bottom',
 
-        items: [
-            {
-                title: 'Dashboard',
-                iconCls: 'home',
+        activeItem: 3,
 
-                styleHtmlContent: true,
-                scrollable: true,
+        items: [{
+            title: 'Dashboard',
+            iconCls: 'home',
 
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'SeptaMobi'
-                }
-            },
-            {
-                title: 'Schedule',
-                iconCls: 'action',
+            styleHtmlContent: true,
+            scrollable: true,
 
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Schedule'
-                    }
-                ]
-            }, {
-                title: 'Trip Planner',
-                iconCls: 'action',
-
-                xtype: 'tripplanner'
+            items: {
+                docked: 'top',
+                xtype: 'titlebar',
+                title: 'SeptaMobi'
             }
-        ]
+        },{
+            title: 'Schedule',
+            iconCls: 'action',
+
+            items: [{
+                docked: 'top',
+                xtype: 'titlebar',
+                title: 'Schedule'
+            }]
+        },{
+            title: 'Trip Planner',
+            iconCls: 'action',
+
+            xtype: 'tripplanner'
+        },{
+            title: 'Trip TPL',
+            iconCls: 'action',
+
+            scrollable: true,
+            
+            items: [{
+                docked: 'top',
+                xtype: 'titlebar',
+                title: 'Trip TPL (Temp)'
+            },{
+                xtype: 'triptpltemp'
+            }]
+        }]
     }
 });

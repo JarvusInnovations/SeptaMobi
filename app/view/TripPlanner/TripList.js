6fdd453b3a00eb79235a5129d5ef0ebf01fcdaad
Ryon Coleman <ryon55@gmail.com>
2013-09-21 21:13:42 -0400

    Trip list styles and tweaks

--- app/view/TripPlanner/TripList.js
+++ app/view/TripPlanner/TripList.js
@@ -1,51 +1,53 @@
 //TODO Show selected options on trip detail
 Ext.define('SeptaMobi.view.TripPlanner.TripList', {
-	extend: 'Ext.Container',
-	xtype: 'triplist',
+    extend: 'Ext.Container',
+    xtype: 'triplist',
 
-	config: {
-		tripPlan: null,
-		itineraries: null,
+    config: {
+        tripPlan: null,
+        itineraries: null,
+        scrollable: true,
 
-		layout: {
-			type: 'vbox'
-		},
-		items: [{
-			xtype: 'container',
-			itemId: 'tripDetails',
-			tpl: [
-				'<dl class="trip-details">',
-	                '<h1>Trip Details</h1>',
-	                '<div class="dli">',
-	                    '<dt>From</dt>',
-	                    '<dd>{fromName}</dd>',
-	                '</div>',
-	                '<div class="dli">',
-	                    '<dt>To</dt>',
-	                    '<dd>{toName}</dd>',
-	                '</div>',
-	                '<div class="dli">',
-	                    '<dt>Departure</dt>',		                    
-	                    '<dd>{departTime:date("m/d/Y g:i A")}</dd>',
-	                '</div>',
-            	'</dl>'
-        	]
-		}, {
-			xtype: 'dataview',
-			flex: 1,
-			store: 'Itineraries',
-			cls: 'itineraries-list',
-			itemCls: 'itinerary-list-item',
-			itemTpl: [
-			    '<div class="itinerary-detail departure">Depart {startTime:date("g:i A")}</div>',
-			    '<div class="itinerary-detail duration">{[ (values.duration/1000/60).toFixed(1) ]}&nbsp;min</div>',
-				'<div class="itinerary-detail walk-distance">Includes {[ (values.walkDistance/3.28084).toFixed(1) ]}&nbsp;ft of walking</div>',
-				'<div class="itinerary-detail transfers">{transfers}&nbsp;transfer<tpl if="transfers!=1">s</tpl></div>',
-			]
-		}]
-	},
+        items: [{
+            xtype: 'container',
+            itemId: 'tripDetails',
+            tpl: [
+                '<dl class="trip-details">',
+                    '<div class="dli">',
+                        '<dt>From</dt>',
+                        '<dd>{fromName}</dd>',
+                    '</div>',
+                    '<div class="dli">',
+                        '<dt>To</dt>',
+                        '<dd>{toName}</dd>',
+                    '</div>',
+                    '<div class="dli">',
+                        '<dt>Departure</dt>',                           
+                        '<dd>{departTime:date("m/d/Y g:i A")}</dd>',
+                    '</div>',
+                '</dl>'
+            ]
+        }, {
+            xtype: 'dataview',
+            store: 'Itineraries',
+            cls: 'itineraries-list list-style',
+            itemCls: 'itinerary-list-item',
+            scrollable: null,
+            itemTpl: [
+                '<div class="itinerary-times">',
+                    '<span class="departure">Depart <time>{startTime:date("g:i a")}</time></span> ',
+                    '<span class="arrival">Arrive <time>{endTime:date("g:i a")}</time></span>',
+                '</div>',
+                '<div class="itinerary-details">',
+                    '<span class="itinerary-detail duration">{[ Math.round(values.duration/1000/60) ]}&nbsp;min</span>, ',
+                    '<span class="itinerary-detail walk-distance">{[ (values.walkDistance*0.000621371).toFixed(1) ]}&nbsp;mi of walking</span>, ',
+                    '<span class="itinerary-detail transfers">{transfers}&nbsp;transfer<tpl if="transfers!=1">s</tpl></span>',
+                '</div>'
+            ]
+        }]
+    },
 
-	updateTripPlan: function(plan) {
-		this.down('#tripDetails').setData(plan);
-	}
+    updateTripPlan: function(plan) {
+        this.down('#tripDetails').setData(plan);
+    }
 });

14b3861b8f517147d53d22053d4b01a8a6b21dd3
Kevin Clough <kevinclough@gmail.com>
2013-10-22 20:34:20 -0400

    Remove params that are causing an issue when deployed

--- app/store/Perks.js
+++ app/store/Perks.js
@@ -12,7 +12,11 @@ Ext.define('SeptaMobi.store.Perks', {
 			url: (window.SeptaMobi_API && SeptaMobi_API.perks) || (location.protocol == 'http:' ? './api/perks' : 'http://appdev.septa.org/perk_data/perk_data.json'),
 			reader: {
 				type: 'json'
-			}
+			},
+			pageParam: false,
+			limitParam: false,
+			startParam: false,
+			noCache: false
 		},
 
 		autoLoad: true

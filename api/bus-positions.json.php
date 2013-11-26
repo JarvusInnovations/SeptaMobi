5fc530bc1309ff413ad53e17cfb2d86979f1a248
Kevin Clough <kevinclough@gmail.com>
2013-11-26 17:19:34 -0500

    remove unneeded headers

--- api/bus-positions.json.php
+++ api/bus-positions.json.php
@@ -2,8 +2,4 @@
 
 include 'HttpProxy.class.php';
 
-// permit CORS
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
-
 HttpProxy::relayRequest('http://www3.septa.org/hackathon/TransitView/'.urlencode($_REQUEST['route']));

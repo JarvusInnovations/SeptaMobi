b7db3a675f4709c763b21005bf953ac213c75eac
Kevin Clough <kevinclough@gmail.com>
2013-12-17 20:15:58 -0500

    Fix subway select issue

--- app/store/Subways.js
+++ app/store/Subways.js
@@ -5,8 +5,12 @@ Ext.define('SeptaMobi.store.Subways', {
 	config: {
 		model: 'SeptaMobi.model.Route',
 		data: [{
+			id: 21442,
+            route_id: "12025",
 			route_long_name: 'Broad Street Line'
 		}, {
+			id: 21442,
+            route_id: "12025",
 			route_long_name: 'Market-Frankford Line'
 		}]
 	}

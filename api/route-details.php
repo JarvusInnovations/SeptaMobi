276cd8dc9ba88889bdf83ac6f1a604983c3811d8
Kevin Clough <kevinclough@gmail.com>
2014-02-13 12:52:45 -0500

    optimize route detail api

--- api/route-details.php
+++ api/route-details.php
@@ -1,16 +1,28 @@
 <?php
 
-$stops = file_get_contents( 'http://next-transit.com/routes/' . $_GET['route_id'] . '/directions/' . $_GET['direction_id'] . '/stops?api_key=' . $_GET['api_key']);
+if($_GET['stop_id']) {
+	$times = file_get_contents( 'http://next-transit.com/routes/' . $_GET['route_id']  . '/directions/' . $_GET['direction_id'] . '/stops/' . $_GET['stop_id'] . '/trips?api_key=' . $_GET['api_key']);
+	$times_data = json_decode($times, true)['data'];
+	$stops_data = array();
+} 
+else {
+	$stops = file_get_contents( 'http://next-transit.com/routes/' . $_GET['route_id'] . '/directions/' . $_GET['direction_id'] . '/stops?api_key=' . $_GET['api_key']);
+	$stops_data = json_decode($stops, true)['data'];
+
+	$times_data = array();
+}
+
 $route_shape = file_get_contents( 'http://next-transit.com/routes/' . $_GET['route_id'] . '/shapes?api_key=' . $_GET['api_key']);
 $alerts = file_get_contents( 'http://www3.septa.org/hackathon/Alerts/get_alert_data.php?req1=' . $_GET['alert_id']);
 
-$stops_data = json_decode($stops, true);
+
 $route_shape_data = json_decode($route_shape, true);
 $alerts_data = json_decode($alerts, true);
 
 header('Content-type: application/json', true);
 print json_encode(array(
-	'stops' => $stops_data['data']
+	'stops' => $stops_data
+	,'times' => $times_data
 	,'shape' => $route_shape_data['data']
 	,'alerts' => $alerts_data
 ));
